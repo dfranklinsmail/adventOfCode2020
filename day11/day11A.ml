75478ea48bd44b3f2e271af2337d50d60cd3b614
Daniel Franklin <dfranklinsmail@gmail.com>
2021-02-28 14:38:04 -0500

    finished day 11A

--- day11/day11A.ml
+++ day11/day11A.ml
@@ -1,68 +1,93 @@
 
 let rec equalsSeatingArea seatingArea anotherSeatingArea =
-             match (seatingArea, anotherSeatingArea) with
-            | [], [] -> true
-            | [], _ -> false
-            | _, [] -> false
-            | (h::t), (hh::tt) -> if String.equal h hh 
-                                then
-                                    equalsSeatingArea t tt
-                                else false
+match (seatingArea, anotherSeatingArea) with
+    | [], [] -> Printf.printf "hit the end\n";true
+    | [], _ -> false
+    | _, [] -> false
+    | (h::t), (hh::tt) -> Printf.printf "comparing %s with %s\n" h hh;
+        if String.equal h hh 
+        then
+            equalsSeatingArea t tt
+        else false
 
-let rec countUnocupiedSeatsInRow row length index =
-    if index = length 
+let rec countUnocupiedSeatsInRow row rowLength index =
+    if index = rowLength 
     then 0
-    else if (String.get row index) = 'L'
-    then 1 + countUnocupiedSeatsInRow row length (index+1)
-    else countUnocupiedSeatsInRow row length (index+1)
+    else if (String.get row index) = '#'
+    then 1 + countUnocupiedSeatsInRow row rowLength (index+1)
+    else countUnocupiedSeatsInRow row rowLength (index+1)
 
 let rec countUnocupiedSeats seatingArea =
     match seatingArea with
     | [] -> 0
     | h::t -> countUnocupiedSeats t + (countUnocupiedSeatsInRow h (String.length h) 0)
 
+let getNeiboursHelperHelper row rowLength colIndex = 
+    if (colIndex < rowLength) && (colIndex >= 0)
+    then
+       [(String.get row colIndex)]
+    else
+      []
 
 let getNeighboursHelper seatingArea rowIndex colIndex includeMe = 
-    if rowIndex < (List.length seatingArea) then
+    let numRows = List.length seatingArea in
+    if rowIndex < numRows && rowIndex >= 0 then
         let row = List.nth seatingArea rowIndex in
-            [(List.nth row colIndex)]
+        let rowLen = String.length row in
+            if includeMe 
+            then
+                List.append (getNeiboursHelperHelper row rowLen (colIndex - 1)) (List.append (getNeiboursHelperHelper row rowLen colIndex) (getNeiboursHelperHelper row rowLen (colIndex + 1)))
+            else
+                List.append (getNeiboursHelperHelper row rowLen (colIndex - 1)) (getNeiboursHelperHelper row rowLen (colIndex + 1))
     else 
         []
 
 (* Find the 8 neighbouring seats next to the given location *)
 let getNeighbours seatingArea rowIndex colIndex =
-    (getNeighboursHelper seatingArea (rowIndex-1) colIndex false)::(getNeighboursHelper seatingArea rowIndex colIndex false)::[(getNeighboursHelper seatingArea (rowIndex+1) colIndex false)]
+   List.append (List.append (getNeighboursHelper seatingArea (rowIndex-1) colIndex true) (getNeighboursHelper seatingArea rowIndex colIndex false)) (getNeighboursHelper seatingArea (rowIndex+1) colIndex true)
 
-let rec noOccupiedAdjacent seatingArea rowIndex colIndex = 
-    let neighbours = getNeighbours seatingArea rowIndex colIndex in
-        List.length neighbours > 1
+let rec count charList listLength char index =      
+    if listLength <= index
+    then 0
+    else
+        let currentChar = (List.nth charList index) in
+        if currentChar = char
+        then 1 + (count charList listLength char (index+1))
+        else count charList listLength char (index+1)
 
-let rec adjacentOccupiedSeatCount seatingArea rowIndex colIndex = 
-    let neighbours = getNeighbours seatingArea rowIndex colIndex in
-        List.length neighbours
+let noOccupiedAdjacent neighbours = 
+    let countOfOccupied = (count neighbours (List.length neighbours) '#' 0) in
+    let answer = countOfOccupied = 0 in
+        answer
+
+let toManyAdjacentOccupied neighbours = 
+    (count neighbours (List.length neighbours) '#' 0) >= 4
+
 
 let rec createNewRow oldRow seatingArea rowLength rowIndex colIndex newRow =
     if rowLength = colIndex
     then newRow
-    else let seat = (String.get oldRow colIndex) in
-        if seat = 'L' && (noOccupiedAdjacent seatingArea rowIndex colIndex) then
+    else 
+        let seat = (String.get oldRow colIndex) in
+        let neighbours = (getNeighbours seatingArea rowIndex colIndex) in
+        if seat = 'L' && (noOccupiedAdjacent neighbours) then
             createNewRow oldRow seatingArea rowLength rowIndex (colIndex+1) (newRow ^ "#")
-        else if seat = '#' && (adjacentOccupiedSeatCount seatingArea rowIndex colIndex) >= 4 then
-            createNewRow oldRow seatingArea rowLength rowIndex (colIndex+1) (newRow ^ "L")
+        else if seat = '#' && (toManyAdjacentOccupied neighbours) then
+           createNewRow oldRow seatingArea rowLength rowIndex (colIndex+1) (newRow ^ "L")
         else
             createNewRow oldRow seatingArea rowLength rowIndex (colIndex+1) (newRow ^ (String.make 1 seat))
 
-let rec applyRules seatingArea index =
-    match seatingArea with
+let rec applyRules remaingRowsToApplyRules originalSeatingArea index =
+    match remaingRowsToApplyRules with
     | [] -> []
-    | h::t -> (createNewRow h seatingArea (String.length h) index 0 "")::(applyRules t (index+1))
+    | h::t -> (createNewRow h originalSeatingArea (String.length h) index 0 "")::(applyRules t originalSeatingArea (index+1))
 
 
 class seatstate (seatingArea : string list) =
     object (self)
         method move =
             (* logic to simulate people arriving and leaving *)
-            new seatstate (applyRules seatingArea 0)
+            new seatstate (applyRules seatingArea seatingArea 0)
 
         method getUnocupiedSeatsCount =
             (* logic for calculating how many empty seats there are in the seating area *)
@@ -70,22 +95,23 @@ class seatstate (seatingArea : string list) =
 
         method getSeatingArea = seatingArea
 
-        method equals seatingArea =
+        method equals otherSeatingArea =
             (* determines if the giving seating state is equal to my seating area *)
-            equalsSeatingArea seatingArea seatingArea
+            equalsSeatingArea seatingArea otherSeatingArea
        
     end
 
-let rec simulateSeating seatState =
+let rec simulateSeating seatState count =
+    Printf.printf "\nsimulating seating area with size %d\n" (List.length seatState#getSeatingArea);
     let newSeatState = seatState#move in
     if seatState#equals newSeatState#getSeatingArea
     then
         newSeatState
     else 
-        simulateSeating newSeatState
+        simulateSeating newSeatState (count+1)
 
 let findUnocupiedSeats input = 
     let seatState = new seatstate input in
-    let steadyState = simulateSeating seatState in
+    let steadyState = simulateSeating seatState 0 in
     let answer = steadyState#getUnocupiedSeatsCount in
         Printf.printf "the answer is %d\n" answer; answer
--- day11/test_day11.ml
+++ day11/test_day11.ml
@@ -11,15 +11,56 @@ let read_lines filename =
         | None -> close_in iChannel; List.rev acc in
     loop []
 
+let rec printNeighbours n = 
+    match n with
+    | [] -> Printf.printf "\n"
+    | h::t -> Printf.printf "%c" h; printNeighbours t
+
+let testNeighbours x y file =    
+    let n = (getNeighbours (read_lines file) x y) in
+        printNeighbours n;
+    let answer = List.length n in
+        Printf.printf "the number of neighbours is %d\n" answer;
+        answer
+
+let testMove x = 
+    let seatCountAfterMove = ((new seatstate (read_lines "test.data"))#move#move#getUnocupiedSeatsCount) in
+        Printf.printf "the seat count after move is %d\n" seatCountAfterMove;
+        seatCountAfterMove
+
 (*TEST SUITE*)
 let tests = "test suite for day 8 part A from Advent of Code 2020 " >::: [
-    "get nearest neibours" >:: (fun _ -> assert_equal 0 (List.length (getNeighbours (read_lines "test.data") 100 0)));
-    "get nearest neibours" >:: (fun _ -> assert_equal 3 (List.length (getNeighbours (read_lines "test.data") 0 0)));
-    "get nearest neibours" >:: (fun _ -> assert_equal 8 (List.length (getNeighbours (read_lines "test.data") 2 3)));
-    (* "get unocupied seat count" >:: (fun _ -> assert_equal 71 ((new seatstate (read_lines "test.data"))#getUnocupiedSeatsCount));
-    "get unocupied seat count" >:: (fun _ -> assert_equal true ((new seatstate (read_lines "test.data"))#equals ((new seatstate (read_lines "test.data"))#getSeatingArea)));
-    "get unocupied seat count" >:: (fun _ -> assert_equal 0 ((new seatstate (read_lines "test.data"))#move#getUnocupiedSeatsCount));
-    "get unocupied seat count" >:: (fun _ -> assert_equal 71 (findUnocupiedSeats (read_lines "test.data"))); *)
+  (*  
+    "get seating area test" >:: (fun _ -> assert_equal true ((new seatstate (read_lines "test.data"))#equals ((new seatstate (read_lines "test.data"))#getSeatingArea)));
+    "test equlas seating area, compare with empty" >:: (fun _ -> assert_equal false (equalsSeatingArea (read_lines "test.data") []));
+    "get unocupied seat count test" >:: (fun _ -> assert_equal 71 ((new seatstate (read_lines "test.data"))#getUnocupiedSeatsCount));
+    "test equlas seating area, compare with empty reverse " >:: (fun _ -> assert_equal false (equalsSeatingArea [] (read_lines "test.data")));
+    "test equlas seating area, compare the same seating area" >:: (fun _ -> assert_equal true (equalsSeatingArea (read_lines "test.data") (read_lines "test.data")));
+    "test equlas seating area, compare the same seating area" >:: (fun _ -> assert_equal false (equalsSeatingArea (read_lines "test.data") ((new seatstate (read_lines "test.data"))#move#getSeatingArea)));
+    "get unocupied seat count after move test" >:: (fun _ -> assert_equal 28 (testMove 1));
+    "get nearest neibours invalid column" >:: (fun _ -> assert_equal 0 (testNeighbours 5 100 "test.data"));
+    "get nearest neibours invalid row" >:: (fun _ -> assert_equal 0 (testNeighbours 100 0 "test.data"));
+    "get nearest neibours top left" >:: (fun _ -> assert_equal 3 (testNeighbours 0 0 "test.data"));
+    "get nearest neibours top middle" >:: (fun _ -> assert_equal 5 (testNeighbours 0 5 "test.data"));
+    "get nearest neibours top right" >:: (fun _ -> assert_equal 3 (testNeighbours 0 9 "test.data"));
+    "get nearest neibours middle left" >:: (fun _ -> assert_equal 5 (testNeighbours 3 0 "test.data"));
+    "get nearest neibours middle right" >:: (fun _ -> assert_equal 5 (testNeighbours 3 9 "test.data"));
+    "get nearest neibours middle middle" >:: (fun _ -> assert_equal 8 (testNeighbours 3 3 "test.data"));
+    "get nearest neibours bottom left" >:: (fun _ -> assert_equal 3 (testNeighbours 9 0 "test.data"));
+    "get nearest neibours bottom right" >:: (fun _ -> assert_equal 3 (testNeighbours 9 9 "test.data"));
+   
+    "no ocupied adjacent test, yes" >:: (fun _ -> assert_equal false (noOccupiedAdjacent ['#'; '#'; '#'; '#'; '#'; '#'; '#'])); 
+    "no ocupied adjacent test, yes just one" >:: (fun _ -> assert_equal false (noOccupiedAdjacent ['#'; 'L'; 'L'; 'L'; 'L'; 'L'; 'L'])); 
+    "no ocupied adjacent test, no" >:: (fun _ -> assert_equal true (noOccupiedAdjacent ['L'; 'L'; 'L'; 'L'; 'L'; 'L'; 'L'])); 
+ 
+    "is adjacent seats ocupied test, yes" >:: (fun _ -> assert_equal true (toManyAdjacentOccupied ['#'; '#'; '#'; '#'; '#'; '#'; '#'])); 
+    "is adjacent seats ocupied test, yes just enough" >:: (fun _ -> assert_equal true (toManyAdjacentOccupied ['#'; '#'; '#'; '#'; 'L'; 'L'; 'L'])); 
+    "is adjacent seats ocupied test, no not enough" >:: (fun _ -> assert_equal false (toManyAdjacentOccupied ['L'; 'L'; 'L'; 'L'; '#'; '#'; '#'])); 
+    "is adjacent seats ocupied test, no" >:: (fun _ -> assert_equal false (toManyAdjacentOccupied ['L'; 'L'; 'L'; 'L'; 'L'; 'L'; 'L'])); 
+    
+    "get nearest neibours top middle" >:: (fun _ -> assert_equal 5 (testNeighbours 9 5 "test2.data"));
+    "get unocupied seat count" >:: (fun _ -> assert_equal 37 (findUnocupiedSeats (read_lines "test.data"))); 
+   *)
+    "get unocupied seat count THE REAL DEAL" >:: (fun _ -> assert_equal 909 (findUnocupiedSeats (read_lines "input.data"))); 
 ]
-
 let _ = run_test_tt_main tests
